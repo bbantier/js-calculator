f0c3aedd7bb69db301be8b91ad560aeb26a27ef3
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-18 15:10:48 +0100

    create html, css and js files with basic functions

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+function add (firstNum, secondNum) {
+  return firstNum + secondNum;
+}
+
+function subtract (firstNum, secondNum) {
+  return firstNum - secondNum;
+}
+
+function multiply (firstNum,secondNum) {
+  return firstNum * secondNum;
+}
+
+function divide (firstNum, secondNum) {
+  return firstNum / secondNum;
+}
